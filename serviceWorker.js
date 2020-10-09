5ed8235ae2ec7dbb63c52f508c1a528fb305ea6b
e-labInnovations <mohammedashad23@gmail.com>
2020-10-09 16:51:35 +0530

    New service worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,44 +1,78 @@
-const CACHE_NAME = "walleto-v1"
-const urlsToCache = ["/index.html", "/css/style.css"]
+const CACHE = "walleto-v1"
+const offlineFallbackPage = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/css/variables.css",
+  "https://cdn.jsdelivr.net/npm/@ionic/core/css/ionic.bundle.css",
+  "https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.esm.js",
+  "https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.js",
+  "js/app.js",
+  "js/icons.js",
+  "js/main.js",
+  "js/pages/Home.js",
+  "js/pages/components/ItemCard.js",
+  "js/pages/components/DetailedItemModal.js"
+  ];
 
 const self = this;
 
-//innstall SW
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(CACHE_NAME)
-        .then((cache) => {
-            console.log('Opened cache');
-
-            return cache.addAll(urlsToCache);
-        })
-    )
+// Install stage sets up the index page (home page) in the cache and opens a new cache
+self.addEventListener("install", function (event) {
+  console.log("Install Event processing");
+ 
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      console.log("Cached offline page during install");
+ 
+      if (offlineFallbackPage === "ToDo-replace-this-name.html") {
+        return cache.add(new Response("Update the value of the offlineFallbackPage constant in the serviceworker."));
+      }
+      
+      return cache.addAll(offlineFallbackPage);
+    })
+  );
 });
-
-//Listen for requests
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(() => {
-                return fetch(event.request) 
-                    .catch(() => caches.match('index.html')) //offline
-            })
-    )
+ 
+// If any fetch fails, it will look for the request in the cache and serve it from there first
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") return;
+ 
+  event.respondWith(
+    fetch(event.request)
+      .then(function (response) {
+        console.log("Add page to offline cache: " + response.url);
+ 
+        // If request was success, add or update it in the cache
+        event.waitUntil(updateCache(event.request, response.clone()));
+ 
+        return response;
+      })
+      .catch(function (error) {        
+        console.log("Network request Failed. Serving content from cache: " + error);
+        return fromCache(event.request);
+      })
+  );
 });
+ 
+function fromCache(request) {
+  // Check to see if you have it in the cache
+  // Return response
+  // If not in the cache, then return error page
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      if (!matching || matching.status === 404) {
+        return Promise.reject("no-match");
+      }
+ 
+      return matching;
+    });
+  });
+}
+ 
+function updateCache(request, response) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.put(request, response);
+  });
+}
 
-//Activate the SW
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [];
-    cacheWhitelist.push(CACHE_NAME);
-
-    event.waitUntil(
-        caches.keys().then((cacheNames) => Promise.all(
-            cacheNames.map((cacheName) => {
-                if(!cacheWhitelist.includes(cacheName)) {
-                    return caches.delete(cacheName);
-                }
-            })
-        ))
-            
-    )
-});
